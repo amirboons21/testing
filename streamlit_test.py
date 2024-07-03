d8a0f4a5c87428ac8f8e31af8b114f5250051dfa
amirboons21 <147031470+amirboons21@users.noreply.github.com>
2024-07-03 09:41:52 +0800

    Update streamlit_test.py
    converted to datetime

--- streamlit_test.py
+++ streamlit_test.py
@@ -8,4 +8,5 @@ Hello *world!*
 
 df = pd.read_csv("msci_world_price_ts.csv")
 df.drop(columns='Unnamed: 0', inplace=True)
+df['Periods'] = pd.to_datetime(df['Periods'], dayfirst=True)
 st.line_chart(df, x='Periods')
